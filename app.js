1b8bc6d68536232b4dd64d54d5c2385cfede2251
Dmitriy <dmitriy.morochkovskiy@gmail.com>
2022-01-20 13:14:58 +0200

    Typo fix, change sorting, clean icons
    app:
    rename variables:
     - startMessage => noStartMessage
     - dataService => data
    rename functions:
     - open() => load();
     - createIn(element) => createTaskTemplate(element);
    rename id of html elements:
     - id="start-message" => id="no-tasts-message";
     - id="show-important" => id="show-important-tasks"
     - <a id="show-not-important" => <a id="show-not-important-tasks"
    simplifye structure of tasks sorting function:
    renamed titles;
    created enter key const
    deleteAllButton moved outside dropdown div
    
    clean icons.css
    
    added "cursor: pointer" on elements

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let input = document.querySelector("#task-input");
 let addTaskButton = document.querySelector("#add-task-btn");
 let taskList = document.querySelector(".task-list");
-let startMessage = document.querySelector("#start-message");
+let noStartMessage = document.querySelector("#no-tasts-message");
 let dropDown = document.querySelector(".dropdown-content");
+let deleteAllButton = document.querySelector("#delete-all-button");
 
 class Task {
   constructor(text) {
@@ -12,7 +13,8 @@ class Task {
   }
 };
 
-let dataService = {
+
+let data = {
   tasks: [],
 
   get allTasks() {
@@ -44,7 +46,7 @@ let dataService = {
     let index = this.tasks.indexOf(task);
     this.tasks.splice(index, 1);
 
-    if (this.tasks.length === 0) taskList.innerHTML = `<p id="start-message">No tasks ...</p>`
+    if (this.tasks.length === 0) taskList.innerHTML = `<p id="no-tasts-message">No tasks ...</p>`
     this.save();
   },
 
@@ -52,83 +54,68 @@ let dataService = {
     localStorage.setItem("tasks", JSON.stringify(this.tasks));
   },
 
-  open() {
+  load() {
     this.tasks = JSON.parse(localStorage.getItem("tasks")) || [];
   }
 };
 
+
 class TasksListView {
   element;
-  dataService;
+  data;
 
   constructor(element) {
     this.element = element;
-    dataService = dataService;
+    data = data;
   }
 
   #drawList(tasksElements) {
     this.element.innerHTML = "";
 
     tasksElements.forEach(taskElement => {
-      taskElement.createIn(this.element);
+      taskElement.createTaskTemplate(this.element);
     });
   }
 
-  drawAll() {
+  showFilteredTasks(target) {
+    let showCompletedButton = document.querySelector("#show-completed-tasks");
+    let showNotCompletedButton = document.querySelector("#show-not-completed-tasks");
+    let showImportantButton = document.querySelector("#show-important-tasks");
+    let showNotImportantButton = document.querySelector("#show-not-important-tasks");
+
     let taskElements = [];
-    let tasks = dataService.allTasks;
-    if (tasks.length === 0) return;
+    let tasks = [];
 
-    tasks.forEach(task => {
-      taskElements.push(new TaskView(task))
-    });
-    this.#drawList(taskElements);
-  }
+    if (data.tasks.length === 0) {
+      return
+    }
+    switch (target) {
+      case showCompletedButton:
+        tasks = data.CompletedTasks;
+        break;
+      case showNotCompletedButton:
+        tasks = data.notCompletedTasks;
+        break;
+      case showImportantButton:
+        tasks = data.importantTasks;
+        break;
+      case showNotImportantButton:
+        tasks = data.notImportantTasks;
+        break;
+      default:
+        tasks = data.allTasks;
+        break;
+    }
 
-  drawCompleted() {
-    let taskElements = [];
-    let tasks = dataService.CompletedTasks;
-    if (tasks.length === 0) return alert("No completed tasks");
 
-    tasks.forEach(task => {
-      taskElements.push(new TaskView(task))
-    });
-    this.#drawList(taskElements);
-  }
-
-  drawNotCompleted() {
-    let taskElements = [];
-    let tasks = dataService.notCompletedTasks;
-    if (tasks.length === 0) return alert("Not completed tasks are missing");
-
-    tasks.forEach(task => {
-      taskElements.push(new TaskView(task))
-    });
-    this.#drawList(taskElements);
-  }
-
-  drawImportant() {
-    let taskElements = [];
-    let tasks = dataService.importantTasks;
-    if (tasks.length === 0) return alert("Not important tasks are missing");
-
-    tasks.forEach(task => {
-      taskElements.push(new TaskView(task))
-    });
-    this.#drawList(taskElements);
-  }
-
-  drawNotImportant() {
-    let taskElements = [];
-    let tasks = dataService.notImportantTasks;
-    if (tasks.length === 0) return alert("No important tasks");
-
+    if (tasks.length === 0) return alert("No  relevant tasks");
     tasks.forEach(task => {
       taskElements.push(new TaskView(task))
     });
     this.#drawList(taskElements);
   }
 };
+
 
 class TaskView {
   constructor(task) {
@@ -136,14 +123,14 @@ class TaskView {
     this.div = null;
   }
 
-  createIn(element) {
+  createTaskTemplate(element) {
     this.div = document.createElement("div");
     this.div.classList.add("task");
 
     let check = document.createElement("i");
     check.classList.add("fa-regular", "fa-circle");
     check.addEventListener("click", this.changeState.bind(this));
-    check.setAttribute("title", "Check this point if you have completed the task");
+    check.setAttribute("title", "Mark as done");
 
     let p = document.createElement("p");
     p.innerText = this.task.text;
@@ -152,17 +139,17 @@ class TaskView {
     star.classList.add("fa-regular");
     star.classList.add("fa-star");
     star.addEventListener("click", this.changePrioriry.bind(this));
-    star.setAttribute("title", "Check this point if you task is important");
+    star.setAttribute("title", "Mark as important");
 
     let deleteButton = document.createElement("i");
     deleteButton.classList.add("fas");
     deleteButton.classList.add("fa-trash-alt");
     deleteButton.addEventListener("click", this.delete.bind(this));
-    deleteButton.setAttribute("title", "Click this button to delete a task");
+    deleteButton.setAttribute("title", "Delete task");
 
     this.div.append(check);
     this.div.append(p);
-    
+
     this.div.append(star);
     this.div.append(deleteButton);
 
@@ -186,7 +173,7 @@ class TaskView {
 
   changeState() {
     this.task.isDone = !this.task.isDone;
-    dataService.save();
+    data.save();
     this.div.classList.toggle("completed");
 
     this.div.childNodes[0].classList.toggle("fa-solid");
@@ -202,38 +189,42 @@ class TaskView {
     this.div.childNodes[2].classList.toggle("fa-solid");
     this.div.childNodes[2].classList.toggle("fa-regular");
 
-    dataService.save();
+    data.save();
   }
 
   delete() {
     if (confirm("Do you really want to delete this task?")) {
-      dataService.delete(this.task);
+      data.delete(this.task);
       this.div.remove();
     }
   }
 };
 
 
-dataService.open();
+data.load();
 let tasksListView = new TasksListView(taskList);
 
 addTaskButton.addEventListener("click", addTaskHandler);
 
 window.addEventListener("load", function () {
-  tasksListView.drawAll();
+  tasksListView.showFilteredTasks();
 });
 
 input.addEventListener("keydown", function (e) {
-  if (e.code === "Enter") addTaskHandler();
+  const ENTER_KEY_CODE = 13;
+  if (e.keyCode === ENTER_KEY_CODE) {
+    addTaskHandler();
+  }
 });
 
 function addTaskHandler() {
   if (input.value) {
-    if (!startMessage.hidden) startMessage.hidden = true;
-
+    if (!noStartMessage.hidden) {
+      noStartMessage.hidden = true;
+    }
     let newTask = new Task(input.value);
-    dataService.add(newTask);
-    tasksListView.drawAll();
+    data.add(newTask);
+    tasksListView.showFilteredTasks();
 
     input.value = "";
   } else {
@@ -243,36 +234,13 @@ function addTaskHandler() {
 
 
 dropDown.addEventListener("click", (e) => {
-  let showAllButton = document.querySelector("#show-all-tasks");
-  let showCompletedButton = document.querySelector("#show-completed-tasks");
-  let showNotCompletedButton = document.querySelector("#show-not-completed-tasks");
-  let showImportantButton = document.querySelector("#show-important");
-  let showNotImportantButton = document.querySelector("#show-not-important");
+  tasksListView.showFilteredTasks(e.target)
+});
 
-  let deleteAllButton = document.querySelector("#delete-all-btn");
-  switch (e.target) {
-    case showAllButton:
-      if (dataService.tasks.length === 0) return alert("No tasks");
-      tasksListView.drawAll();
-      break;
-    case showCompletedButton:
-      tasksListView.drawCompleted();
-      break;
-    case showNotCompletedButton:
-      tasksListView.drawNotCompleted();
-      break;
-    case showImportantButton:
-      tasksListView.drawImportant();
-      break;
-    case showNotImportantButton:
-      tasksListView.drawNotImportant();
-      break;
-    case deleteAllButton:
-      if (confirm("Are you sure you want to delete everything?")) {
-        localStorage.clear();
-        taskList.innerHTML = `<p id="start-message">No tasks ...</p>`;
-        dataService.tasks = [];
-      }
-      break
+deleteAllButton.addEventListener("click", () => {
+  if (confirm("Are you sure you want to delete everything?")) {
+    localStorage.clear();
+    taskList.innerHTML = `<p id="no-tasts-message">No tasks ...</p>`;
+    data.tasks = [];
   }
 });
